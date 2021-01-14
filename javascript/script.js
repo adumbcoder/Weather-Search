8c2d36855fd38bb7402f18d4e4b7e7ff3c1abb3b
adumbcoder <70477253+adumbcoder@users.noreply.github.com>
2021-01-13 23:44:52 -0500

    update

--- javascript/script.js
+++ javascript/script.js
@@ -4,16 +4,19 @@
 
 //api.openweathermap.org/data/2.5/weather?q={city name},{state code},{country code}&appid={API key}
 function populateWeather(){
-let search = document.querySelector('#search-value');
+let search = document.querySelector('#search-value').textContent;
 let apiKey = '{781f26b7b30722bc9d7b5d602eebcb7f}';
-let queryURL = 'https://www.openweathermap.org/data/2.5/weather?q=' + search.textContent + '&appid=' + apiKey;
+let queryURL = 'https://www.openweathermap.org/data/2.5/weather?q=' + search + '&appid=' + apiKey;
 
 
-$.ajax({
+$.ajax(
+    {
     url:queryURL,
     method:"GET"
     
-}).then(function(response){
+}
+).then(function(response)
+{
     console.log(response);
     // let newCityHome = document.createElement('div');
     // let newCityName = document.createElement('h2')
@@ -23,8 +26,10 @@ $.ajax({
 
 
 document.addEventListener('click', function(e){
-    e.target.match('.search-button')
+    if(e.target.match('.search-button'))
+    {
     populateWeather();
+    };
 });
 //city name, date, an icon for the weather, temperature, humidty, wind speed, UV index
 
